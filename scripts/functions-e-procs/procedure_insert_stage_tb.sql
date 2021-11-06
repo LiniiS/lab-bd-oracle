8c291efb876f11e6415491b2b5a1a7ff803cffad
LiniiS <enila404@gmail.com>
2021-11-06 17:44:28 -0300

    update proc da Stage table

--- scripts/functions-e-procs/procedure_insert_stage_tb.sql
+++ scripts/functions-e-procs/procedure_insert_stage_tb.sql
@@ -1,23 +1,24 @@
--- Proc para inserção na Tabela Stage v1
-drop procedure proc_stage;
+-- Proc para inserção na Tabela Stage v2
+-- Procedure pra popular a stage
+ -- drop procedure proc_stage;
   create procedure proc_stage as
     begin
         -- truncate table stage_table;
         delete from stage_table; -- preferencialmente usar o truncate se o oracleXE deixar
         insert into stage_table
         
-        select prd_id as ID_PRODUTO, prd_nome as PRODUTO, prd_preco as PRECO_PRODUTO, vnd_id as ID_VENDA, vnd_valor_total as VALOR_TOTAL, VND_FUN_ID as VENDEDOR_ID, fun_primeiro_nome || ' ' || fun_ultimo_nome as FUNCIONARIO, crg_nome as CARGO, for_estado as ORIGEM_FORNECEDOR, cat_nome as CATEGORIA_PRODUTO, 
+        select prd_nome as PRODUTO, prd_preco as PRECO_PRODUTO, itv_quantidade as QTD_VENDIDA, cat_nome as CATEGORIA_PRD, fun_primeiro_nome || ' ' || fun_ultimo_nome as FUNCIONARIO, for_estado as ORIGEM_FORNECEDOR, 
     
      -- decode(coluna, valor 1, retorno 1, valor 2, retorno 2...default)   
-        decode ( substr(vend1.vnd_doc_cliente, 14), ' ', 'CPF', 'CNPJ'),
+        decode ( length(vend1.vnd_doc_cliente), 11, 'CPF', 'CNPJ') as TIPO_CLIENTE,
+
+        vnd_valor_total as VALOR_TOTAL,
         
         (case
             when to_char(vend1.vnd_dt_hr, 'HH24') < 12 then 'MANHÃ'
             else 'TARDE'
             end
             ) as PERIODO_VENDA,
-        --   extract(hour from cast(vnd_dt_hr as timestamp)) as hora_da_venda,
-        --  TO_CHAR((vnd_dt_hr),'dd-Mon'),
            
         (case
             when to_char(vnd_dt_hr, 'mm') in (1,2,3) then 1
@@ -25,7 +26,9 @@ drop procedure proc_stage;
             when to_char(vnd_dt_hr, 'mm') in (7,8,9) then 3
             when to_char(vnd_dt_hr, 'mm') in (10,11,12) then 4
             end 
-            ) as TRIMESTRE
+            ) as TRIMESTRE,
+            to_char(vnd_dt_hr, 'DAY') as DIA_SEMANA
+            
         
     from
         itens_venda
@@ -34,9 +37,9 @@ drop procedure proc_stage;
         left  join funcionarios func1 on(vend1.vnd_fun_id = func1.fun_id)
         left  join fornecedores for1 on(for1.for_id = prod1.prd_for_id)
         left  join categorias cat1 on(prod1.prd_cat_id = cat1.cat_id)
-        left  join cargos carg1 on(func1.fun_crg_id = carg1.crg_id);
-
     end;
+
+
          
 -- rotina Tabela Stage
 -- execute proc_stage();
